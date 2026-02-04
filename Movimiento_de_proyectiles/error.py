28b949a3d44bcdcb323e89ebbe8fd8713e91b6cc
Jorge <jorbanejahuerta@gmail.com>
2026-02-04 16:57:52 +0100

    projectile_motion terminado. Toca errores. Escribir. Dejar bonitas las graficas.

--- Movimiento_de_proyectiles/error.py
+++ Movimiento_de_proyectiles/error.py
@@ -1,1 +1,46 @@
-#Calculamos el error cometido en nuestra animacion, comparando
+import numpy as np
+import matplotlib.pyplot as plt
+
+#                                                  Analitical aproximations for range and time of flight
+#Aprox k --> min
+
+T_aprox = []
+R_aprox = []
+T = []
+R = []
+k1 = list(np.linspace(0, 0.1, 10))
+for k in k1:
+    if k == 0:
+        T_aprox.append(2*v0y/g)
+        R_aprox.append(v0x*(2*v0y/g))
+        T.append(2*v0y/g)
+        R.append(v0x*(2*v0y/g))
+    else:
+        T_aprox.append(2*v0y/g*(1 - k*v0y / (3*g)))
+        R_aprox.append(v0x*(T_aprox[-1] - 0.5*k*T_aprox[-1]**2))
+
+        #Solving transcendental equation for T -- numerically
+        func = lambda t: t - (k*v0y+g)/(g*k)*(1 - np.exp(-k*t))
+        T_real = fsolve(func, T_aprox[-1])[0]
+        T.append(T_real)
+        R.append(v0x*T_real)
+
+
+
+plt.figure()
+plt.plot(k1, T, label='Real time')
+plt.plot(k1, T_aprox, label='Approximated time')
+plt.xlabel('k')
+plt.ylabel('Time (s)')
+plt.legend()
+plt.grid()
+plt.show()
+
+plt.figure()
+plt.plot(k1, R, label='Real range')
+plt.plot(k1, R_aprox, label='Approximated range')
+plt.xlabel('k')
+plt.ylabel('Range (m)')
+plt.legend()
+plt.grid()
+plt.show()
--- Movimiento_de_proyectiles/projectile_motion.py
+++ Movimiento_de_proyectiles/projectile_motion.py
@@ -2,22 +2,96 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import fsolve
 
-g = 9.81
+#################################################   Initial parameters   ###################
+
+g = 9.81 #Gravity
 x0, y0, grades, v0 = 0, 0, 45, 30 # Initial condition
 t = np.linspace(0, 10, 100) #Time
-alpha = grades*np.pi/ 180
+alpha = np.radians(grades)
+dt = t[1] - t[0] #Step
 k_value = [0, 0.05, 0.1, 0.2, 0.5]
 v0x = v0*np.cos(alpha)
 v0y = v0*np.sin(alpha)
+e = 0.8 #Rebote 0 < e < 1
+
+################################################# Movement of projectiles through different media
+
+#  Analitical
 x = np.zeros((len(t), len(k_value)))
 y = np.zeros((len(t), len(k_value)))
 
-e = 0.8 #Rebote 0 < e < 1
+for i, k in enumerate(k_value): 
+    if k == 0: 
+        x[:, i] = v0x * t + x0 
+        y[:, i] = -0.5*g*t**2 + v0y*t + y0 
 
-#In this kind of motion the acceleration parameterer is function of velocity through this equations:
+        y_col = y[:, i]
+        idx_ground = np.where(y_col < 0)[0]
 
+        if len(idx_ground) > 0:
+            first_hit = idx_ground[0]
+            y_col[first_hit:] = 0
+            continue
 
-# al final de cada k
+    else: 
+        x[:, i] = v0x/k * (1 - np.exp(-k*t)) + x0 
+        y[:, i] = -g*t/k + (k*v0y + g)/k**2 * (1 - np.exp(-k*t)) + y0
+
+        y_col = y[:, i]
+        idx_ground = np.where(y_col < 0)[0]
+
+        if len(idx_ground) > 0:
+            first_hit = idx_ground[0]
+            y_col[first_hit:] = 0
+            continue
+
+# Numerical Euler's method
+
+x_numeric = np.zeros((len(t), len(k_value)))
+y_numeric = np.zeros((len(t), len(k_value)))
+
+for i, k in enumerate(k_value):
+    x_pos = x0
+    y_pos = y0
+    vx = v0x
+    vy = v0y
+    for j in range(len(t)):
+
+        x_numeric[j][i] = x_pos
+        y_numeric[j][i] = y_pos
+
+        #Current velocity
+        vx += - k * vx * dt
+        vy += - g * dt - k * vy * dt
+        # Current position
+        x_pos += vx * dt
+        y_pos += vy * dt
+
+        if y_pos < 0:
+            y_pos = 0
+            continue
+
+#  Plot
+
+fig1, axs1 = plt.subplots(2, 2, figsize = (10, 8))
+axs1 = axs1.flatten()
+for idx, k in enumerate(k_value[1:]):   
+    col = idx + 1 
+
+    axs1[idx].plot(x[:,col], y[:,col], label="Exacta")
+    axs1[idx].plot(x_numeric[:, col], y_numeric[:, col], '--', label="Euler")
+
+    axs1[idx].set_title(f"k = {k}")
+    axs1[idx].set_xlabel("x (m)")
+    axs1[idx].set_ylabel("y (m)")
+    axs1[idx].grid()
+    axs1[idx].legend()
+
+plt.tight_layout()
+
+################################################ Rebound movement
+
+# Analitical
 x_totales =[]
 y_totales =[]
 
@@ -82,35 +156,9 @@ for i, k in enumerate(k_value):
     
     x_totales.append(np.concatenate(x_list_rebot))
     y_totales.append(np.concatenate(y_list_rebot))
+  
 
-
-
-        
-for i, k in enumerate(k_value): 
-    if k == 0: 
-        x[:, i] = v0x * t + x0 
-        y[:, i] = -0.5*g*t**2 + v0y*t + y0 
-
-        y_col = y[:, i]
-        idx_ground = np.where(y_col < 0)[0]
-
-        if len(idx_ground) > 0:
-            first_hit = idx_ground[0]
-            y_col[first_hit:] = 0
-
-    else: 
-        x[:, i] = v0x/k * (1 - np.exp(-k*t)) + x0 
-        y[:, i] = -g*t/k + (k*v0y + g)/k**2 * (1 - np.exp(-k*t)) + y0#
-
-        y_col = y[:, i]
-        idx_ground = np.where(y_col < 0)[0]
-
-        if len(idx_ground) > 0:
-            first_hit = idx_ground[0]
-            y_col[first_hit:] = 0
-#                                                    Numerical error (Euler's method) - ref animation.py
-
-dt = t[1] - t[0]
+# Numerical
 
 x_approx = np.zeros((len(t), len(k_value)))
 y_approx = np.zeros((len(t), len(k_value)))
@@ -139,7 +187,6 @@ for i, k in enumerate(k_value):
             vy = 0
             break
 
-
 fig, axs = plt.subplots(2,2, figsize=(10, 8))
 axs = axs.flatten()
 
@@ -161,49 +208,4 @@ for idx, k in enumerate(k_value[1:]):
     axs[idx].legend()
 
 plt.tight_layout()
-plt.show()
-
-
-#                                                  Analitical aproximations for range and time of flight
-#Aprox k --> min
-
-T_aprox = []
-R_aprox = []
-T = []
-R = []
-k1 = list(np.linspace(0, 0.1, 10))
-for k in k1:
-    if k == 0:
-        T_aprox.append(2*v0y/g)
-        R_aprox.append(v0x*(2*v0y/g))
-        T.append(2*v0y/g)
-        R.append(v0x*(2*v0y/g))
-    else:
-        T_aprox.append(2*v0y/g*(1 - k*v0y / (3*g)))
-        R_aprox.append(v0x*(T_aprox[-1] - 0.5*k*T_aprox[-1]**2))
-
-        #Solving transcendental equation for T -- numerically
-        func = lambda t: t - (k*v0y+g)/(g*k)*(1 - np.exp(-k*t))
-        T_real = fsolve(func, T_aprox[-1])[0]
-        T.append(T_real)
-        R.append(v0x*T_real)
-
-
-
-plt.figure()
-plt.plot(k1, T, label='Real time')
-plt.plot(k1, T_aprox, label='Approximated time')
-plt.xlabel('k')
-plt.ylabel('Time (s)')
-plt.legend()
-plt.grid()
-plt.show()
-
-plt.figure()
-plt.plot(k1, R, label='Real range')
-plt.plot(k1, R_aprox, label='Approximated range')
-plt.xlabel('k')
-plt.ylabel('Range (m)')
-plt.legend()
-plt.grid()
 plt.show()
