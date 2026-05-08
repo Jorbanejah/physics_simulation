b6e8e825c1c04e8f13ac523cc06d8cced372a17f
Jorge <jorbanejahuerta@gmail.com>
2026-05-08 12:48:17 +0200

    Graphics structures

--- oscillatory_motion/special_oscillation/double_pendulum/main_graphics.py
+++ oscillatory_motion/special_oscillation/double_pendulum/main_graphics.py
@@ -1,1 +1,21 @@
+"""
+MAIN GRPAHICS: double pendulum
 
+Small angles
+- Drift energy (E(t) - E(0)) vs intial angle (theta1) vs intial angle (theta2) (heatmap - after a long time)
+- Phase space
+- Verlet vs rk45 vs DOP853 methods
+
+Motion
+- Drift energy vs large time
+- Fractal motion
+- Lyapunov coefficient
+- Poincare sections
+
+Another performance:
+- Convergence 
+- Stability
+- Normal modes animation (small angles): Symmetric mode and Antisymmetric mode
+- Trajectory + Poincaré section side-by-side: Shows how the chaotic cloud emerges from the trajectory.
+
+"""
